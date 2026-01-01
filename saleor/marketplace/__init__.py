f23c84bda06bd8e5e6c9e6e036ad8e405366214c
Sabu J <mail@jsabu.com>
2026-01-01 14:50:24 +0530

    Export models_loyalty and utils_loyalty from marketplace __init__.py

--- saleor/marketplace/__init__.py
+++ saleor/marketplace/__init__.py
@@ -1,5 +1,15 @@
 """Marketplace app for multi-vendor functionality."""
 
+# Import submodules to make them available for direct import
+from . import error_codes
+from . import models
+from . import models_loyalty
+from . import utils_loyalty
 
-
+__all__ = [
+    "error_codes",
+    "models",
+    "models_loyalty",
+    "utils_loyalty",
+]
 
